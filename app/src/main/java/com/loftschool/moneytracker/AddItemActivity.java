ed40c262795f4b714659723dbab29c8f0e413b1e
Anna Sedykh <anna_sedykh@mail.ru>
2018-03-12 02:56:19 +0300

    * ruble sign always display with price * add_btn enabled when both name and price not empty

---
+++ app/src/main/java/com/loftschool/moneytracker/AddItemActivity.java
@@ -0,0 +1,67 @@
+package com.loftschool.moneytracker;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.TextUtils;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class AddItemActivity extends AppCompatActivity {
+
+    private static final String TAG = "AddItemActivity";
+    private static final String RUBLE_SIGN = "\u20BD";
+
+    private EditText name;
+    private EditText price;
+    private Button addBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_additem);
+
+        name = findViewById(R.id.name);
+        price = findViewById(R.id.price);
+        addBtn = findViewById(R.id.add_btn);
+
+        TextWatcher textWatcher = new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                if (!TextUtils.isEmpty(price.getText()) && !price.getText().toString().endsWith(RUBLE_SIGN)) {
+                    price.setText(price.getText() + RUBLE_SIGN);
+                    price.setSelection(price.length() - 1);
+                }
+                if (price.getText().toString().equals(RUBLE_SIGN)) {
+                    price.setText("");
+                }
+
+                addBtn.setEnabled(!TextUtils.isEmpty(name.getText()) && !TextUtils.isEmpty(price.getText()));
+            }
+        };
+
+        name.addTextChangedListener(textWatcher);
+        price.addTextChangedListener(textWatcher);
+
+
+        addBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String itemName = name.getText().toString();
+                String itemPrice = price.getText().toString();
+            }
+        });
+    }
+}
